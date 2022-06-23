8e9deeae18782c7b1f4a1e2ea71b55a9be5ba28c
Mauro Paulino de Paiva JR <mauro.paulino@gmail.com>
2022-06-23 03:04:58 -0300

    Operations with count, find e any

--- src/main/kotlin/com/estudos/collections/Teste OperacoesArray.kt
+++ src/main/kotlin/com/estudos/collections/Teste OperacoesArray.kt
@@ -17,5 +17,19 @@ fun main() {
 
     salarioMaiorQue2000.forEach { println(it) }
 
+    println("----------------------")
+    println("Contar - Count -  dentro de um range de salarios")
+    println(salariosOf.count { it in 1500.0..2600.0 })
+
+    println("----------------------")
+    println("Procurar - Find - salarios")
+    println(salariosOf.find { it == 2580.78 })
+    println(salariosOf.find { it == 1200.0 }) // exemplo de salario nao existente no array
+
+    println("----------------------")
+    println("Uso do ANY") // retorna true ou false como resultado da pesquisa
+    println(salariosOf.any { it == 1100.0 })
+    println(salariosOf.any { it == 1999.0 })
+
 
 }
